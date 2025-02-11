4e5cb7a4bea1f0904d8ec7e9b25424775e552a7c
Christopher <contato.christopher@gmail.com>
2025-02-10 21:13:50 -0300

    run biome to format

--- aula-1.ts
+++ aula-1.ts
@@ -1,9 +1,9 @@
 function somar(a: number, b: number): number | string {
   if (a < b) {
-    return a + b //somar
+    return a + b; //somar
   } else {
-    return '' + a + b //concatenar
+    return '' + a + b; //concatenar
   }
 }
-console.log('Resultado:', somar(1, 2))
-console.log('Resultado:', somar(5, 3))
+console.log('Resultado:', somar(1, 2));
+console.log('Resultado:', somar(5, 3));
